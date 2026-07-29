54766fa9ff32f2569deede7eb24a8ba3a573fc57
Gustavo <gxrctripp@gmail.com>
2026-07-29 16:31:10 -0400

    asdasd11131

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,10 @@
 <script>
-    import axios from "axios";
-
-    const tracks = axios.get("https://leonardoapi.vercel.app/api/tracks")
-
+const {data} = $props()
 </script>
 
-{#await tracks}
-    Espera socio... 
-{:then respuesta} 
-    {#each respuesta.data.tracks as tracks}
+    {#each data.tracks as tracks}
     <div class="p-2">
         <p class="font-bold">{tracks.title}</p>
         <p class="text-gray-500">{tracks.album.title}</p>
     </div>    
     {/each}
-{/await}
