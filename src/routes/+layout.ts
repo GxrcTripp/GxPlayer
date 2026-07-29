adea72f3780eb6d2c8d4935238b4ce35e400a742
Gustavo <gxrctripp@gmail.com>
2026-07-29 16:22:02 -0400

    ad12

--- src/routes/+layout.ts
+++ src/routes/+layout.ts
@@ -2,4 +2,8 @@ import axios from "axios";
 
 export const load = async () => {
     const tracks = await axios.get("https://leonardoapi.vercel.app/api/tracks")
+
+     return {
+        tracks: tracks.data.tracks
+     }
 }
