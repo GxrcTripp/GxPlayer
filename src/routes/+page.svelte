0f5268bb87e614279438ac729435f0ff267971a0
Gustavo <gxrctripp@gmail.com>
2026-07-29 16:10:41 -0400

    Creacion del reproductor de musica

---
+++ src/routes/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    import axios from "axios";
+
+    const tracks = axios.get("https://leonardoapi.vercel.app/api/tracks")
+
+</script>
+
+{#await tracks}
+    Espera socio... 
+{:then respuesta} 
+    {#each respuesta.data.tracks as tracks}
+    <div class="p-2">
+        <p class="font-bold">{tracks.title}</p>
+        <p class="text-gray-500">{tracks.album.title}</p>
+    </div>    
+    {/each}
+{/await}
