a0d35ff9892bb9cdd6c44ca5eaa98e4431b5e2f8
Gustavo <gxrctripp@gmail.com>
2026-08-05 14:45:48 -0400

    lenis jjj

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,21 @@
 <script lang="ts">
-  import './layout.css';
-  import Navbar from '$lib/components/Navbar.svelte';
-  import PlayerBar from '$lib/components/PlayerBar.svelte';
-
+  import "./layout.css";
+  import Navbar from "$lib/components/Navbar.svelte";
+  import PlayerBar from "$lib/components/PlayerBar.svelte";
+  import Lenis from "lenis";
+  import { onMount } from "svelte";
   let { children } = $props();
+
+  onMount(() => {
+    const lenis = new Lenis({
+      autoRaf: true,
+    });
+  });
 </script>
 
-<div class="h-screen w-screen bg-[#0D0F12] text-[#e2e2e6] flex flex-col overflow-hidden relative">
+<div
+  class="h-screen w-screen bg-[#0D0F12] text-[#e2e2e6] flex flex-col overflow-hidden relative"
+>
   <!-- Área de Sidebar + Contenido Principal de cada ruta -->
   <div class="flex flex-1 overflow-hidden pb-20">
     <Navbar />
